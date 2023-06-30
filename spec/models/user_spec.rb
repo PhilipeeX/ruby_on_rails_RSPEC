dd5d236365eca1bebfc4a90d3fe3bb20b970ece1
Philipe <dev.philipe@outlook.com>
2023-06-30 08:58:10 -0300

    test: test created using factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,7 @@ require 'ffaker'
 
 RSpec.describe User, type: :model do
   it 'is invalid if the level isn\'t between 1 and 99' do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
+    user = build(:user, level: FFaker::Random.rand(100..999))
     expect(user).not_to be_valid
   end
   it 'return the correct hero title' do
