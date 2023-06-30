56804aee198062bd3ca2a9f47905bfec4fd59ed5
Philipe <dev.philipe@outlook.com>
2023-06-30 15:16:19 -0300

    test: some tests created to weapon

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'weapons/index'
+  get 'weapons/create'
+  get 'weapons/delete'
+  get 'weapons/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
